eadb5b0139a567cd894590def29ccb0f7506c2e4
midoistory <midoriharahap483@gmail.com>
2024-10-02 11:24:02 +0700

    Dashboard Index done

--- app/Http/Controllers/admin/DashboardController.php
+++ app/Http/Controllers/admin/DashboardController.php
@@ -4,14 +4,23 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Dashboard;
+use App\Models\Achievement;
+use App\Models\News;
+use App\Models\Subject;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
 class DashboardController extends Controller
 {
+    // Mengambil Total Data Achievement
     public function index()
     {
-        return view('pages.admin.dashboard.index');
+        $totalAchievement = Achievement::count();
+        $totalNews = News::count();
+        $totalSubject = Subject::count();
+
+        // Kirim data ke view
+        return view('pages.admin.dashboard.index', compact('totalAchievement', 'totalNews', 'totalSubject'));
     }
 }
 
--- resources/views/pages/admin/dashboard/index.blade.php
+++ resources/views/pages/admin/dashboard/index.blade.php
@@ -23,10 +23,10 @@
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                 Total Achievement</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $totalAchievement }}</div>
                         </div>
                         <div class="col-auto">
-                            <i class="fas fa-trophy fa-2x text-300"></i>
+                            <i class="fas fa-trophy fa-2x text-300" style="color: #386cc7"></i>
                         </div>
                     </div>
                 </div>
@@ -40,27 +40,10 @@
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                 Total News</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $totalNews }}</div>
                         </div>
                         <div class="col-auto">
-                            <i class="fas fa-newspaper fa-2x text-300" style="color: #1d4fa5" ></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-primary shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                Total Alumni</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                            <i class="fas fa-newspaper fa-2x text-300" style="color: #386cc7"></i>
                         </div>
                     </div>
                 </div>
@@ -74,10 +57,27 @@
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                 Total Subject</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $totalSubject }}</div>
                         </div>
                         <div class="col-auto">
-                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                            <i class="fas fa-book fa-2x text-300" style="color: #386cc7"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                Total Alumni</div>
+                            {{-- <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $totalAlumni }}</div> --}}
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-user-graduate fa-2x text-300" style="color: #386cc7"></i>
                         </div>
                     </div>
                 </div>
@@ -91,10 +91,10 @@
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                 Total Staff</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+                            {{-- <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $totalStaff }}</div> --}}
                         </div>
                         <div class="col-auto">
-                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                            <i class="fas fa-address-card fa-2x text-300" style="color: #386cc7"></i>
                         </div>
                     </div>
                 </div>
@@ -108,10 +108,10 @@
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                 Total Project</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+                            {{-- <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $totalProject }}</div> --}}
                         </div>
                         <div class="col-auto">
-                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                            <i class="fas fa-diagram-project fa-2x text-300" style="color: #386cc7"></i>
                         </div>
                     </div>
                 </div>
@@ -125,10 +125,10 @@
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                 Total Comment</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+                            {{-- <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $totalComment }}</div> --}}
                         </div>
                         <div class="col-auto">
-                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                            <i class="fas fa-comment fa-2x text-300" style="color: #386cc7"></i>
                         </div>
                     </div>
                 </div>
