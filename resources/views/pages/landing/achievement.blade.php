47390a415e85f4977fb64822f879570ed5df49a2
midoistory <midoriharahap483@gmail.com>
2024-10-19 09:01:40 +0700

    baselanding template ok

--- resources/views/pages/landing/achievement.blade.php
+++ resources/views/pages/landing/achievement.blade.php
@@ -2,10 +2,13 @@
 @extends('pages.landing.index')
 
 @section('content')
-    <main class="space-y-40 mb-40">
+    <main class="mt-10 space-y-40 mb-40">
         <div class="relative">
+            <br>
+            <br>
+            <br>
             <div>
-                <div class="max-w-7xl mx-auto px-6 md:px-12 xl:px-6">
+                <div class="max-w-7xl mx-auto px-6 md:px-12 xl:px-6 py-5">
                     <div class="mb-12 space-y-2 text-center">
                         <h2 class="text-3xl font-bold text-gray-800 md:text-4xl dark:text-white">Our Achievement</h2>
                         <p class="lg:mx-auto lg:w-6/12 text-gray-600 dark:text-gray-300">
@@ -16,43 +19,24 @@
 
                     <div class="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
                         <div
-                            class="group p-6 sm:p-8 rounded-3xl bg-white border border-gray-100 dark:shadow-none dark:border-gray-700 dark:bg-gray-800 bg-opacity-50 shadow-2xl shadow-gray-600/10">
-                            <div class="relative overflow-hidden rounded-xl">
-                                <img src="https://images.unsplash.com/photo-1661749711934-492cd19a25c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80"
-                                    alt="art cover" loading="lazy" width="1000" height="667"
-                                    class="h-64 w-full object-cover object-top transition duration-500 group-hover:scale-105">
-                            </div>
-                            <div class="mt-6 relative">
-                                <h3 class="text-2xl font-semibold text-gray-800 dark:text-white">
-                                    Judul Achievement
-                                </h3>
-                                <p class="mt-6 mb-8 text-gray-600 dark:text-gray-300">
-                                    Voluptates harum aliquam totam, doloribus eum impedit atque! Temporibus...
-                                </p>
-                                <a class="inline-block" href="#">
-                                    <span class="text-info dark:text-blue-300">Read more</span>
-                                </a>
-                            </div>
-                        </div>
-                        <div
-                            class="group p-6 sm:p-8 rounded-3xl bg-white border border-gray-100 dark:shadow-none dark:border-gray-700 dark:bg-gray-800 bg-opacity-50 shadow-2xl shadow-gray-600/10">
-                            <div class="relative overflow-hidden rounded-xl">
-                                <img src="https://images.unsplash.com/photo-1491895200222-0fc4a4c35e18?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80"
-                                    alt="art cover" loading="lazy" width="1000" height="667"
-                                    class="h-64 w-full object-cover object-top transition duration-500 group-hover:scale-105">
-                            </div>
-                            <div class="mt-6 relative">
-                                <h3 class="text-2xl font-semibold text-gray-800 dark:text-white">
-                                    De fuga fugiat lorem ispum laboriosam expedita.
-                                </h3>
-                                <p class="mt-6 mb-8 text-gray-600 dark:text-gray-300">
-                                    Voluptates harum aliquam totam, doloribus eum impedit atque! Temporibus...
-                                </p>
-                                <a class="inline-block" href="#">
-                                    <span class="text-info dark:text-blue-300">Read more</span>
-                                </a>
-                            </div>
 
+                            class="group p-6 sm:p-6 rounded-3xl bg-white border border-gray-100 dark:shadow-none dark:border-gray-700 dark:bg-gray-800 bg-opacity-50 shadow-2xl shadow-gray-600/10">
+                            <div class="relative overflow-hidden rounded-xl">
+                                <img src="https://images.unsplash.com/photo-1620121692029-d088224ddc74?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80"
+                                    alt="art cover" loading="lazy" width="1000" height="667"
+                                    class="h-48 w-full object-cover object-top transition duration-500 group-hover:scale-105">
+                            </div>
+                            <div class="mt-6 relative">
+                                <h3 class="text-xl font-semibold text-gray-800 dark:text-white">
+                                    De fuga fugiat lorem ispum laboriosam expedita.
+                                </h3>
+                                <p class="mt-2 mb-8 text-gray-600 dark:text-gray-300">
+                                    Voluptates harum aliquam totam, doloribus eum impedit atque! Temporibus...
+                                </p>
+                                <a class="inline-block" href="#">
+                                    <span class="text-info dark:text-blue-300">Read more</span>
+                                </a>
+                            </div>
                         </div>
                         <div
                             class="group p-6 sm:p-8 rounded-3xl bg-white border border-gray-100 dark:shadow-none dark:border-gray-700 dark:bg-gray-800 bg-opacity-50 shadow-2xl shadow-gray-600/10">
@@ -62,7 +46,7 @@
                                     class="h-64 w-full object-cover object-top transition duration-500 group-hover:scale-105">
                             </div>
                             <div class="mt-6 relative">
-                                <h3 class="text-2xl font-semibold text-gray-800 dark:text-white">
+                                <h3 class="text-xl font-semibold text-gray-800 dark:text-white">
                                     De fuga fugiat lorem ispum laboriosam expedita.
                                 </h3>
                                 <p class="mt-6 mb-8 text-gray-600 dark:text-gray-300">
@@ -81,7 +65,26 @@
                                     class="h-64 w-full object-cover object-top transition duration-500 group-hover:scale-105">
                             </div>
                             <div class="mt-6 relative">
-                                <h3 class="text-2xl font-semibold text-gray-800 dark:text-white">
+                                <h3 class="text-xl font-semibold text-gray-800 dark:text-white">
+                                    De fuga fugiat lorem ispum laboriosam expedita.
+                                </h3>
+                                <p class="mt-6 mb-8 text-gray-600 dark:text-gray-300">
+                                    Voluptates harum aliquam totam, doloribus eum impedit atque! Temporibus...
+                                </p>
+                                <a class="inline-block" href="#">
+                                    <span class="text-info dark:text-blue-300">Read more</span>
+                                </a>
+                            </div>
+                        </div>
+                        <div
+                            class="group p-6 sm:p-8 rounded-3xl bg-white border border-gray-100 dark:shadow-none dark:border-gray-700 dark:bg-gray-800 bg-opacity-50 shadow-2xl shadow-gray-600/10">
+                            <div class="relative overflow-hidden rounded-xl">
+                                <img src="https://images.unsplash.com/photo-1620121692029-d088224ddc74?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80"
+                                    alt="art cover" loading="lazy" width="1000" height="667"
+                                    class="h-64 w-full object-cover object-top transition duration-500 group-hover:scale-105">
+                            </div>
+                            <div class="mt-6 relative">
+                                <h3 class="text-xl font-semibold text-gray-800 dark:text-white">
                                     De fuga fugiat lorem ispum laboriosam expedita.
                                 </h3>
                                 <p class="mt-6 mb-8 text-gray-600 dark:text-gray-300">
@@ -95,7 +98,6 @@
                     </div>
                 </div>
             </div>
-
         </div>
     </main>
 @endsection
